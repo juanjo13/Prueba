84ed93ff5ca9e11efac366003ec2d2b650bff27d
Juan Jose Jaquez <juanjjaquezh@hotmail.com>
2019-03-06 18:13:28 -0600

    si subio

--- gebci-api/grails-app/conf/spring/resources.groovy
+++
@@ -1,7 +0,0 @@
-import mx.gob.fgjez.gebci.security.UserPasswordEncoderListener
-
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
-}
-
